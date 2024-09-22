f5c834e315fa2f4398512627efe1faa645bb3e4d
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-09-22 23:18:46 +0530

    UI design Completed

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/SideBar/Sidebar.jsx"
+import MessageContainer from "../components/messages/MessageContainer.jsx"
+const Homepage = () => {
+  return (
+    <>
+    <Sidebar/>
+    <MessageContainer/>
+    </>
+  )
+}
+
+export default Homepage

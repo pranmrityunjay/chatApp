8ab47e9a1aa1c9bea25169784b6bdfa78f5ddb1e
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-11-26 16:43:26 +0530

    changed source

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["https://chat-app-pranmrityunjays-projects.vercel.app"],
 		methods: ["GET", "POST"],
 	},
 });

582ce039a6164caabf4da503e9a1f771a7248df8
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-11-26 14:16:53 +0530

    update server.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", 
+        target: "https://chatapp-g1z5.onrender.com", 
         changeOrigin: true, 
       },
     },
